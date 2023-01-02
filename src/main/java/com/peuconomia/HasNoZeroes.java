d4cf5a83938aec71c1fa8273bf9b9e7540792437
Suman Adhikari <expertxkid@gmail.com>
2023-01-02 17:57:09 +0545

    feat: add HasNoZeroes.java

---
+++ src/main/java/com/peuconomia/HasNoZeroes.java
@@ -0,0 +1,47 @@
+package com.peuconomia;
+
+import java.util.Arrays;
+
+public class HasNoZeroes {
+
+
+    public static void main(String[] args) {
+        int[][] inputArrays =
+                new int[][]{
+                        new int[]{1, 2, 3, },
+                        new int[]{1, 0,4,0 },
+                        new int[]{1, 2, 3,0 },
+                        new int[]{0,0,0,0 },
+                        new int[]{ },
+
+                };
+
+
+
+        int[] expected = new int[]{1,0,0,0,1};
+
+
+        printOutput(inputArrays, expected);
+    }
+
+    public static int hasNoZeroes(int a[]) {
+
+
+        for(int num:a){
+            if(num == 0) return 0;
+        }
+
+       return 1;
+
+
+    }
+
+
+    public static void printOutput(int[][] inputs, int[] expected) {
+
+        for (int i = 0; i < inputs.length; i++) {
+            System.out.printf("N: %-20s Expected: %-10d HasNoZeroes: %-10d\n", Arrays.toString(inputs[i]), expected[i], hasNoZeroes(inputs[i]));
+        }
+
+    }
+}
